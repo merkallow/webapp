9ef41ee749fbd5400900db1e9a441013362ced10
okeken <okekehinde@gmail.com>
2022-03-08 17:41:14 +0100

    chores/typings

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-const Footer: NextPage = () => {
+const Header: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
---
+++ global.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-blockies';
+interface Window {
+    ethereum: any,
+    web3: any,  
+}
